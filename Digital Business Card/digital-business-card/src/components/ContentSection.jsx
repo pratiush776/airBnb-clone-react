31f15d90631d86b096e8700a543aa593cdc8508e
pratiush776 <144255196+pratiush776@users.noreply.github.com>
2024-08-14 14:43:16 -0400

    Delete Digital Business Card directory

--- Digital Business Card/digital-business-card/src/components/ContentSection.jsx
+++
@@ -1,8 +0,0 @@
-export default function ContentSection({title,text}){
-    return (
-        <div className="section">
-            <h1 className="section-title">{title}</h1>
-            <p className="section-text">{text}</p>
-        </div>
-    )
-}
--- Digital Business Card/digital-business-card/src/components/LinkButtons.jsx
+++
@@ -1,5 +0,0 @@
-export default function LinkButton({text,link}){
-    return (
-        <button className="link-button"><a href={link}>{text}</a></button>
-    )
-}
